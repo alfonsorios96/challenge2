71b0067e40c240a16411fcedd7d52e1d9736aba5
Alfonso Rios <malforime@gmail.com>
2017-07-20 17:41:33 -0500

    Se agregan comentarios

---
+++ js/main.js
@@ -0,0 +1,129 @@
+'use strict';
+// Create a last element property for arrays.
+if (!Array.prototype.last) {
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    };
+}
+;
+
+var comments = [];
+
+/**
+ * @description This method saves in the array {comments} a search.
+ */
+function saveComment() {
+    var comment = document.getElementById('comment-input').value;
+    if (comments.length !== 0) {
+        comments.push({id: comments.last().id + 1, data: comment});
+    } else {
+        comments.push({id: 1, data: comment});
+    }
+    if (comment.length == 0) {
+        comments.pop();
+    }
+    document.getElementById('comment-input').value = '';
+    paintComments();
+}
+
+/**
+ * @description Show the div with the recomendations.
+ */
+function paintComments() {
+    var list = document.getElementById('comments-list');
+    list.innerHTML = '';
+    comments.forEach(function (iterator) {
+        addItem(list, iterator);
+    });
+}
+
+/**
+ * @description Insert a li web element into the ul (list of searches)
+ * @param listElement: {id: number, data: string}[] - Array with the comments
+ * @param valueElement: The item iterator has display value and id for delete it.
+ */
+function addItem(listElement, valueElement) {
+    var randomUser = getRandomPeople();
+    console.log(randomUser);
+    var avatar = '<img width="30" height="30" src="' + randomUser.profile_picture + '"> ';
+    var userName = '<strong>'+randomUser.name+'</strong> &#09;&#09;';
+    var newItem = document.createElement('p');
+    var newContent = document.createTextNode('');
+    newItem.appendChild(newContent);
+    newItem.innerHTML += avatar;
+    newItem.innerHTML += userName;
+    newItem.innerHTML += valueElement.data;
+    listElement.appendChild(newItem);
+}
+
+function getRandomPeople() {
+    var peoples = [
+        {
+            id: 1,
+            name: 'Joselyn Morales',
+            profile_picture: 'img/profiles/2.jpg'
+        },
+        {
+            id: 2,
+            name: 'Eboni Rodriquez',
+            profile_picture: 'img/profiles/1.jpg'
+        },
+        {
+            id: 3,
+            name: 'Sadie Bush',
+            profile_picture: 'img/profiles/3.jpg'
+        },
+        {
+            id: 4,
+            name: 'Frank D. Camp',
+            profile_picture: 'img/profiles/4.jpg'
+        },
+        {
+            id: 5,
+            name: 'Timothy E. Duvall',
+            profile_picture: 'img/profiles/5.jpg'
+        },
+        {
+            id: 6,
+            name: 'Clifford J. Enos',
+            profile_picture: 'img/profiles/6.jpg'
+        },
+        {
+            id: 7,
+            name: 'Eleanor P. Parkman',
+            profile_picture: 'img/profiles/7.jpg'
+        },
+        {
+            id: 8,
+            name: 'Wilma L. Jackson',
+            profile_picture: 'img/profiles/8.jpg'
+        },
+        {
+            id: 9,
+            name: 'Jacqueline P. Deatherage',
+            profile_picture: 'img/profiles/9.jpg'
+        },
+        {
+            id: 10,
+            name: 'Edwin E. Jones',
+            profile_picture: 'img/profiles/10.jpg'
+        },
+        {
+            id: 11,
+            name: 'Eboni Rodriquez',
+            profile_picture: 'img/profiles/11.jpg'
+        },
+        {
+            id: 12,
+            name: 'Eboni Rodriquez',
+            profile_picture: 'img/profiles/12.jpg'
+        }];
+    return peoples[getRandomArbitrary(0, peoples.length - 1)];
+}
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+    return (Math.floor(Math.random() * (max - min + 1)) + min);
+}
